01c78062d50abc4ceb415410fa98efec0ed3f569
noumbisdenccformation <noumbissiej@proton.me>
2025-09-21 18:39:13 +0100

    racine projet du group O

---
+++ Backend/src/test/java/com/Group_O/ProjetGestionDeStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group_O;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetGestionDeStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
